7ff301e80ab31513626d99656f052319fb3eb83b
vagrant <vagrant@localhost.localdomain>
2020-08-22 04:04:51 +0000

    first

---
+++ config/routes.rb
@@ -0,0 +1,7 @@
+Rails.application.routes.draw do
+
+  devise_for :admins, controllers: { sessions: 'admins/sessions' }
+  devise_for :users
+	root to: 'homes#top'
+  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+end
