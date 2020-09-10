4807c5d3417f108f8ba1e0fd7ec66ab0f4afdf6c
vagrant <vagrant@localhost.localdomain>
2020-09-10 08:27:00 +0000

    手直し

--- app/controllers/admin_users_controller.rb
+++ app/controllers/admin_users_controller.rb
@@ -7,6 +7,7 @@ class AdminUsersController < ApplicationController
 	def show
 		@user = User.find(params[:id])
   	    @reviews = @user.reviews
+  	                    .all.page(params[:page]).per(10)
 	end
 
   private
